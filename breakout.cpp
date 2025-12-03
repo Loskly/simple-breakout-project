18eab4ffba213b4219a8b01f5a213b955ac140f7
check1 <aa13996@auca.kg>
2025-12-03 18:05:13 +0600

    s

--- breakout.cpp
+++ breakout.cpp
@@ -10,34 +10,62 @@
 void update()
 {
     // TODO
+    bool ifwinckeck = false;
 
-    if (IsKeyPressed(KEY_ESCAPE)) {
-        game_state = paused_state;
-    }
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-        move_paddle(-paddle_speed);
-    }
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-        move_paddle(paddle_speed);
-    }
-    move_ball();
-    if (!is_ball_inside_level()) {
-        load_level();
-        PlaySound(lose_sound);
-    } else if (current_level_blocks == 0) {
-        load_level(1);
-        PlaySound(win_sound);
+    switch (game_state) {
+    case menu_state:
+        if (IsKeyPressed(KEY_ENTER)) {
+            game_state = in_game_state;
+        }
+    case in_game_state:
+
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            game_state = paused_state;
+        }
+        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+            move_paddle(-paddle_speed);
+        }
+        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+            move_paddle(paddle_speed);
+        }
+        move_ball();
+        if (!is_ball_inside_level()) {
+            load_level();
+            PlaySound(lose_sound);
+        } else if (current_level_blocks == 0) {
+            load_level(1);
+            PlaySound(win_sound);
+        }
+         break;
+    case paused_state:
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            game_state = in_game_state;
+        }
+        break;
+        default:;
     }
 }
 
 void draw()
 {
     // TODO
+    switch (game_state) {
+    case menu_state:
+        draw_menu();
+        break;
+    case in_game_state:
+        draw_level();
+        draw_paddle();
+        draw_ball();
+        draw_ui();
+        break;
+    case paused_state:
+        draw_pause_menu();
+        break;
+    default:;
+    }
 
-    draw_level();
-    draw_paddle();
-    draw_ball();
-    draw_ui();
+
 }
 
 int main()
@@ -53,7 +81,7 @@ int main()
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-
+        SetExitKey(KEY_DELETE);
         draw();
         update();
 
